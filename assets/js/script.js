d3e40a1b30241e5f20e5cb1d8e437cde86c6cf43
Nirmal Ravidas <nirmalproject17@gmail.com>
2024-08-05 00:12:02 +0530

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,23 +1,5 @@
 'use strict';
 
-window.addEventListener('load', () => {
-          registerSW();
-        });
-     
-        // Register the Service Worker
-        async function registerSW() {
-          if ('serviceWorker' in navigator) {
-            try {
-              await navigator
-                    .serviceWorker
-                    .register('serviceworker.js');
-            }
-            catch (e) {
-              console.log('SW registration failed');
-            }
-          }
-        }
-
 // element toggle function
 const elementToggleFunc = function (elem) { elem.classList.toggle("active"); }
 
